b8b042a9b44928e0a35a3efa0f940c4388c3ee4e
Ali Hasan <alihasandeveloper@gmail.com>
2025-01-14 12:09:18 +0600

    custom post type added

--- inc/post-type.php
+++ inc/post-type.php
@@ -1,1 +1,37 @@
 <?php
+
+
+function swiper_gallery_post_type()
+{
+    $labels = array(
+        'name'               => _x( 'Galleries', 'gallery' ),
+        'singular_name'      => _x( 'Photo', 'gallery' ),
+        'add_new'            => _x( 'Add New', 'Photo' ),
+        'add_new_item'       => __( 'Add New Photo' ),
+        'edit_item'          => __( 'Edit Photo' ),
+        'new_item'           => __( 'New Photo' ),
+        'all_items'          => __( 'All Photos' ),
+        'view_item'          => __( 'View Photos' ),
+        'search_items'       => __( 'Search Photos' ),
+        'not_found'          => __( 'No Photos found' ),
+        'not_found_in_trash' => __( 'No Photos found in the Trash' ),
+        'parent_item_colon'  => '',
+        'menu_name'          => 'Swiper Gallery'
+    );
+
+    $args = array(
+        'labels'        => $labels,
+        'description'   => '',
+        'public'        => true,
+        'show_ui'        => true,
+        'capability_type'  => 'post',
+        'menu_position' => 26,
+        'supports'      => array( 'title' , 'thumbnail'),
+        'has_archive'   => true,
+        'menu_icon'     => 'dashicons-format-gallery',
+    );
+
+    register_post_type( 'gallery', $args );
+}
+
+add_action('init', 'swiper_gallery_post_type');
